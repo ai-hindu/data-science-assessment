8eb115fc6edc42a28d3e09327139723f9628b9e1
ai-hindu <72295905+ai-hindu@users.noreply.github.com>
2025-11-08 04:25:26 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,13 @@
+SELECT
+investor_transactions.Investor_id,sectors.sector_name,
+ROUND(
+  (investor_transactions.no_of_shares*100.0/
+   SUM(investor_transactions.no_of_shares)
+   OVER(PARTITION BY investor_transactions.investor_id)),
+  2
+) AS share_percentage 
+FROM investor_transactions
+JOIN sectors
+    ON investor_transactions.sector_id = sectors.sector_id
+ORDER BY investor_transactions.investor_id, share_percentage DESC
 -- ADD YOUR SQL QUERY HERE
